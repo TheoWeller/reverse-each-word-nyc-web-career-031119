2ddbfc45da67304b0b0885051fd783ab57f4d5fd
Theo Weller <weller.theodor@gmail.com>
2019-02-23 18:23:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,12 @@ def reverse_each_word(string)
   split_string = string.split()
   split_string.each do |x|
     x.reverse!
-
-  end
+end
   split_string.join(" ")
+end
+
+def expect_any_instance_of(Array)
+  Array.collect do |x|
+    x.reverse!
+  end
 end
